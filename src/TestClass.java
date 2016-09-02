135ca5d213892ef9182d77569afd360dec5f380d
Abdullah <Abdullah@AdmiralAkbarsPC>
2016-09-02 13:54:49 -0500

    Another awesome commit by Abdullah

---
+++ src/TestClass.java
@@ -0,0 +1,18 @@
+//Abdullah Akbar is so sick. 
+
+
+//Like actually.
+public class TestClass {
+
+	public void main(String[] arg){
+		System.out.print("I got it");
+		try {
+		    Thread.sleep(500);                 
+		} catch(InterruptedException ex) {
+		    Thread.currentThread().interrupt();
+		}
+		System.out.print("Cuz I'm awesome");
+
+	}
+	
+}
